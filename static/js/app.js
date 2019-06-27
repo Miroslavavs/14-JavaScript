1104e3c4c8bc8b4f8b360724b2d07a0af836b71a
Miros <villa.miros@hotmail.com>
2019-06-27 13:12:46 -0500

    first commit

---
+++ static/js/app.js
@@ -0,0 +1,34 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+
+  var tbody = d3.select("tbody");
+  filteredData.forEach((date) => {
+    var row = tbody.append("tr");
+    Object.entries(date).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+});
